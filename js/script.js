4a794c487f58ce784637e51cf5a2646112387090
Ishan Taparia <dualstrike99@gmail.com>
2015-04-04 22:40:36 -0500

    FAQ Info added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ var color_one = false;
 var color_two = false;
 var color_three = false;
 var color_four = false;
+var color_five = false;
+var color_six = false;
+var color_seven = false;
+var color_eight = false;
+var color_nine = false;
+var color_ten = false;
+var color_eleven = false;
+var color_twelve = false;
 
 function changeColor_one()
 {
@@ -22,12 +30,12 @@ function changeColor_two()
 {
 	if(color_two == false)
 	{
-		document.getElementById("second_acor").style.backgroundColor = "#0383B3";	
+		document.getElementById("2nd_acor").style.backgroundColor = "#0383B3";	
 		color_two = true;
 	}
 	else if(color_two == true)
 	{
-		document.getElementById("second_acor").style.backgroundColor = "#00ADF7";
+		document.getElementById("2nd_acor").style.backgroundColor = "#00ADF7";
 		color_two = false;
 	}
 }
@@ -36,12 +44,12 @@ function changeColor_three()
 {
 	if(color_three == false)
 	{
-		document.getElementById("third_acor").style.backgroundColor = "#0383B3";	
+		document.getElementById("second_acor").style.backgroundColor = "#0383B3";	
 		color_three = true;
 	}
 	else if(color_three == true)
 	{
-		document.getElementById("third_acor").style.backgroundColor = "#00ADF7";
+		document.getElementById("second_acor").style.backgroundColor = "#00ADF7";
 		color_three = false;
 	}
 }
@@ -50,12 +58,124 @@ function changeColor_four()
 {
 	if(color_four == false)
 	{
-		document.getElementById("fourth_acor").style.backgroundColor = "#0383B3";	
+		document.getElementById("third_acor").style.backgroundColor = "#0383B3";	
 		color_four = true;
 	}
 	else if(color_four == true)
 	{
-		document.getElementById("fourth_acor").style.backgroundColor = "#00ADF7";
+		document.getElementById("third_acor").style.backgroundColor = "#00ADF7";
 		color_four = false;
+	}
+}
+
+function changeColor_five()
+{
+	if(color_five == false)
+	{
+		document.getElementById("fourth_acor").style.backgroundColor = "#0383B3";	
+		color_five = true;
+	}
+	else if(color_five == true)
+	{
+		document.getElementById("fourth_acor").style.backgroundColor = "#00ADF7";
+		color_five = false;
+	}
+}
+
+function changeColor_six()
+{
+	if(color_six == false)
+	{
+		document.getElementById("fifth_acor").style.backgroundColor = "#0383B3";	
+		color_six = true;
+	}
+	else if(color_six == true)
+	{
+		document.getElementById("fifth_acor").style.backgroundColor = "#00ADF7";
+		color_six = false;
+	}
+}
+
+function changeColor_seven()
+{
+	if(color_seven == false)
+	{
+		document.getElementById("sixth_acor").style.backgroundColor = "#0383B3";	
+		color_seven = true;
+	}
+	else if(color_seven == true)
+	{
+		document.getElementById("sixth_acor").style.backgroundColor = "#00ADF7";
+		color_seven = false;
+	}
+}
+
+function changeColor_eight()
+{
+	if(color_eight == false)
+	{
+		document.getElementById("seventh_acor").style.backgroundColor = "#0383B3";	
+		color_eight = true;
+	}
+	else if(color_eight == true)
+	{
+		document.getElementById("seventh_acor").style.backgroundColor = "#00ADF7";
+		color_eight = false;
+	}
+}
+
+function changeColor_nine()
+{
+	if(color_nine == false)
+	{
+		document.getElementById("eigth_acor").style.backgroundColor = "#0383B3";	
+		color_nine = true;
+	}
+	else if(color_nine == true)
+	{
+		document.getElementById("eigth_acor").style.backgroundColor = "#00ADF7";
+		color_nine = false;
+	}
+}
+
+function changeColor_ten()
+{
+	if(color_ten == false)
+	{
+		document.getElementById("ninth_acor").style.backgroundColor = "#0383B3";	
+		color_ten = true;
+	}
+	else if(color_ten == true)
+	{
+		document.getElementById("ninth_acor").style.backgroundColor = "#00ADF7";
+		color_ten = false;
+	}
+}
+
+function changeColor_eleven()
+{
+	if(color_eleven == false)
+	{
+		document.getElementById("tenth_acor").style.backgroundColor = "#0383B3";	
+		color_eleven = true;
+	}
+	else if(color_eleven == true)
+	{
+		document.getElementById("tenth_acor").style.backgroundColor = "#00ADF7";
+		color_eleven = false;
+	}
+}
+
+function changeColor_twelve()
+{
+	if(color_twelve == false)
+	{
+		document.getElementById("eleventh_acor").style.backgroundColor = "#0383B3";	
+		color_twelve = true;
+	}
+	else if(color_twelve == true)
+	{
+		document.getElementById("eleventh_acor").style.backgroundColor = "#00ADF7";
+		color_twelve = false;
 	}
 }
